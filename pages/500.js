30f2efc0af0225eae74976a934d375e3179647e0
Imran Khairuddin <imran_khairuddin@yahoo.com>
2025-05-07 14:22:59 +0800

    Update 500.js

--- pages/500.js
+++ pages/500.js
@@ -1,9 +1,7 @@
-// pages/500.js
 export default function Custom500() {
   return (
     <div style={{ textAlign: 'center', marginTop: '10rem' }}>
-      <h1>500 - Server Error</h1>
-      <p>Oops! Something went wrong.</p>
+      <h1>500 - Server-side error occurred</h1>
     </div>
-  )
+  );
 }
