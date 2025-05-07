0a7026559f4a09c014763bc2716632ab32bee9a1
Imran Khairuddin <imran_khairuddin@yahoo.com>
2025-05-07 12:01:13 +0800

    Create 500.js

---
+++ pages/500.js
@@ -0,0 +1,9 @@
+// pages/500.js
+export default function Custom500() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "10rem" }}>
+      <h1>500 - Internal Server Error</h1>
+      <p>Something went wrong.</p>
+    </div>
+  );
+}
