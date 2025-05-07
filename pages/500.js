464e24bc2217d251c5f09825bd7df04cfd0e95b2
Imran Khairuddin <imran_khairuddin@yahoo.com>
2025-05-07 12:09:37 +0800

    Add Prettier-compliant 500 error page

--- pages/500.js
+++ pages/500.js
@@ -1,9 +1,9 @@
 // pages/500.js
 export default function Custom500() {
   return (
-    <div style={{ textAlign: "center", marginTop: "10rem" }}>
-      <h1>500 - Internal Server Error</h1>
-      <p>Something went wrong.</p>
+    <div style={{ textAlign: 'center', marginTop: '10rem' }}>
+      <h1>500 - Server Error</h1>
+      <p>Oops! Something went wrong.</p>
     </div>
-  );
+  )
 }
